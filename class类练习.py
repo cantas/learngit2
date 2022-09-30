038f9650101ef78194abd047d6664332c28d0c8e
cantas <348696205@qq.com>
2022-09-30 21:20:03 +0800

    Add files via upload

---
+++ class类练习.py
@@ -0,0 +1,10 @@
+class Animal():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def name(self):
+        print("animal222 name333")
+
+c = Animal("apple","sh")
+print(c.name)
